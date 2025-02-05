d0f7fbed409bf2098951d47cd071201b2f205f57
levi-darasa <levi@darasa.io>
2025-02-05 21:14:52 +0700

    add more test api

--- processor/src/main/java/com/betting/transaction/processor/controller/TestController.java
+++ processor/src/main/java/com/betting/transaction/processor/controller/TestController.java
@@ -1,16 +1,30 @@
 package com.betting.transaction.processor.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("")
+@RequiredArgsConstructor
 public class TestController {
+
+
+    private final ReactiveRedisTemplate<Integer, BigDecimal> reactiveRedisTemplate;
 
     @GetMapping("")
     public String test() {
         return "Test controller";
+    }
+
+    @GetMapping("redis")
+    public Mono<BigDecimal> testRedis() {
+        return reactiveRedisTemplate.opsForValue().get(1);
     }
 
 }
